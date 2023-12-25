da0ba7734a00301a12d2c5aeba429aec211a46e4
Vimalbharath <vimalbharath21@gmail.com>
2023-12-25 20:42:11 +0530

    prime using while

--- basics/Prime2.java
+++ basics/Prime2.java
@@ -1,24 +1,26 @@
 package basics;
+
 import java.util.Scanner;
 
-public class Prime {
-    public static void main(String[] args) {
+public class Prime2 {
+     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         System.out.println("Enter the number:");
         int num=input.nextInt();
-        int count=1;
-        for (int i=2;i<num;i++){
-            if (num%i==0){
-               count++;
+        int c=2;
+        while (c<num){
+            if (num%c==0){
+               System.out.println("Not a Prime number");
+               return;
             }
+            c++;
             
         }
-        if (count > 1){
-            System.out.println("Not a Prime number"); 
-        }
-        else{
+        
+        if(c>=num){
                 System.out.println("Prime number");  
             }
         input.close();
     }
+    
 }
