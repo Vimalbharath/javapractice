7c2ed0e082db98c65acfcba2c22e45131ca15a53
Vimalbharath <vimalbharath21@gmail.com>
2023-12-25 22:20:39 +0530

    reverse

--- basics/gCount.java
+++ basics/gCount.java
@@ -8,12 +8,16 @@ public class gCount {
         System.out.println("Enter the number:");
         int num=input.nextInt();
         int count=0;
+        int ans=0;
         while(num>0){
             int rem=num%10;
             if (rem==3)
             {count++;}
+            ans=ans*10+rem;
             num=num/10;
         }
         System.out.println(count);
+        System.out.println(ans);
+
      }
 }
