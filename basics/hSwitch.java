495d95e9481240aeaf5e37103afff1fc5fbc1ed3
Vimalbharath <vimalbharath21@gmail.com>
2023-12-25 23:06:26 +0530

    normal switch

--- basics/hSwitch.java
+++ basics/hSwitch.java
@@ -8,10 +8,21 @@ public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         System.out.println("Enter the number:");
         int num=input.nextInt(); 
+        // switch(num){
+        // case 1,2,3,4,5 -> System.out.println("Weekday");
+        // case 6,7 -> System.out.println("Weekoff");
+        // }
         switch(num){
-        case 1,2,3,4,5 -> System.out.println("Weekday");
-        case 6,7 -> System.out.println("Weekoff");
+            case 1:
+            System.out.println("Weekday");
+            break;
+            case 2:
+            System.out.println("Weekend");
+            break;
+            default:
+            System.out.println("enter valid number");
         }
+
     }
 
 }
