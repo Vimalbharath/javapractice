e8a8c9e442f5ef2ce5a450d39f9d9f0346fc61c4
Vimalbharath <vimalbharath21@gmail.com>
2023-12-25 15:36:31 +0530

    io

---
+++ basics/code2.java
@@ -0,0 +1,18 @@
+package basics;
+
+import java.util.Scanner;
+
+public class code2 {
+    public static void main(String[] args){
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter the values :");
+        int a = scan.nextInt();
+        scan.nextLine();
+       
+         String b=scan.nextLine();
+        System.out.println(a+b);
+        scan.close();
+
+    }
+    
+}
