f0ace44df342e38d644a5b55192f66b3b272132e
Vimalbharath <vimalbharath21@gmail.com>
2023-12-31 13:45:53 +0530

    case copy

--- hackerrank/string/aCapital.java
+++ hackerrank/string/aCapital.java
@@ -13,7 +13,9 @@ public class aCapital {
         String B=sc.next();
         /* Enter your code here. Print output to STDOUT. */
         System.out.println(A.length()+B.length());
-        System.out.println(A.length()+B.length());
+        System.out.println(A.compareTo(B)>0?"Yes":"No");
+        System.out.print(Character.toUpperCase(A.charAt(0))+(A.substring(1))+" ");
+        System.out.print(Character.toUpperCase(B.charAt(0))+(B.substring(1)));
                 
     }
 }
