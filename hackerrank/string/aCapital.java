0648319bd2c453a5a67ac9c2c1eee270330c9857
Vimalbharath <vimalbharath21@gmail.com>
2023-12-31 13:39:56 +0530

    capital

---
+++ hackerrank/string/aCapital.java
@@ -0,0 +1,22 @@
+package hackerrank.string;
+
+
+    
+import java.io.*;
+import java.util.*;
+public class aCapital {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length()+B.length());
+        System.out.println(A.length()+B.length());
+                
+    }
+}
+
+
+
