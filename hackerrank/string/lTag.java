71a49a43ca94df903a4c3ab25f0c7690d979f192
Vimalbharath <vimalbharath21@gmail.com>
2024-01-01 20:24:59 +0530

    tag extractor

---
+++ hackerrank/string/lTag.java
@@ -0,0 +1,26 @@
+package hackerrank.string;
+
+public class lTag{
+    public static void main(String[] args){
+		
+        String regex="<(.+)>([^<]+)</\\1>";
+        Pattern pattern=Pattern.compile(regex);
+        
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String line = in.nextLine();
+			
+          	//Write your code here
+			Matcher matcher=pattern.matcher(line);
+            boolean found=false;
+            while(matcher.find()){
+                System.out.println(matcher.group(2));
+                found=true;
+            }
+            if(!found) System.out.println("None");
+            
+			testCases--;
+		}
+	}
+}
