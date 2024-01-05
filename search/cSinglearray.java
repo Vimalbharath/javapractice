979c360d2d52f75cfe1a5b5187870264e0f6640e
Vimalbharath <vimalbharath21@gmail.com>
2024-01-05 21:53:39 +0530

    string should be entered in quotes

---
+++ search/cSinglearray.java
@@ -0,0 +1,17 @@
+package search;
+
+import java.util.Arrays;
+
+public class cSinglearray {
+    public static void main(String[] args) {
+        
+    
+    int[] arr={1,2,3,4,5};
+    System.out.println(Arrays.toString(arr));
+    System.out.println(arr[1]);
+    String[] str={"a","b","cc","dd"};
+    System.out.println(str[1]);
+    System.out.println(Arrays.toString(str));
+
+    }
+}
