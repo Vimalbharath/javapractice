cba31db0c8eb4771d8facd5465d6284858ce4fc1
Vimalbharath <vimalbharath21@gmail.com>
2024-01-05 23:38:42 +0530

    adding -1 next to array length

--- search/fMax.java
+++ search/fMax.java
@@ -14,7 +14,7 @@ public class fMax {
 
     static int[] reverse(int[] arr) {
        int start= 0;
-       int end= arr.length;
+       int end= arr.length-1;
        while(start<end){
         swap(arr,start,end);
         start++;
