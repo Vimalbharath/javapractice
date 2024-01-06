6ec7259083959d6e74aec8a681ac47688a924a46
Vimalbharath <vimalbharath21@gmail.com>
2024-01-06 20:48:21 +0530

    debug mid should be inside while loop need to add equals

--- searchbinary/binarysearch.java
+++ searchbinary/binarysearch.java
@@ -11,16 +11,19 @@ public class binarysearch {
     private static int binarySearch(int[] arr, int target) {
         int start=0;
         int end=arr.length-1;
-        int mid=start+(end-start)/2;
+       
         while(start<end){
-            if(arr[mid]==target){
-                return mid;
-            }
-            if(arr[mid]<=target){
+             int mid=start+(end-start)/2;
+            
+            if(arr[mid]<target){
                 start=mid+1;
             }
             if(arr[mid]>target){
                 end=mid-1;
+            }
+            if(arr[mid]==target){
+                int a=arr[mid];
+                return mid;
             }
         }
         return -1;
