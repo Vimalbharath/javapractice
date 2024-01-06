ad7773823aadad78d27dfa33f66af0ef1ffc309c
Vimalbharath <vimalbharath21@gmail.com>
2024-01-06 13:20:12 +0530

    arraylist declaration and multi dimension pending

---
+++ search/gList.java
@@ -0,0 +1,16 @@
+package search;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class gList {
+    public static void main(String[] args) {
+    ArrayList<Integer> vimal= new ArrayList<>(5);
+    vimal.add(1);
+    vimal.add(1);
+    vimal.add(1);
+    vimal.add(1);
+    System.out.println(vimal);
+    
+    }
+}
