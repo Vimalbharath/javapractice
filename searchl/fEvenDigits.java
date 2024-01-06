4e0d92f1d2411ba70a3297077a8459f2abe6650c
Vimalbharath <vimalbharath21@gmail.com>
2024-01-06 19:39:57 +0530

    trying debug

--- searchl/fEvenDigits.java
+++ searchl/fEvenDigits.java
@@ -1,22 +1,30 @@
 package searchl;
 
-class Solution {
-    public int findNumbers(int[] nums) {
+import java.util.Arrays;
+
+class fEvenDigits {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+    System.out.println(Arrays.toString(arr));
+    int a=findNumbers(arr);
+    System.out.println(a);
+    }
+    public static int findNumbers(int[] nums) {
         int count=0;
         for(int i=0;i<nums.length;i++){
-            if(iseven(num)){
+            if(iseven(nums[i])){
                 count++;
             }
         }
         return count;
     }
-    public boolean iseven(int num){
-        if (digit(num)%2==0){
+    public static boolean iseven(int num){
+        if (digits(num)%2==0){
             return true;
         }
         return false;
     }
-    public int digits(int num){
-        return Math.log10(num);
+    public static int digits(int num){
+        return (int)Math.log10(num);
     }
 }
