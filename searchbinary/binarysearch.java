9fc75dcf9da5c6fb5e84427c8c195be2d65e6735
Vimalbharath <vimalbharath21@gmail.com>
2024-01-06 21:01:10 +0530

    while equals

--- searchbinary/binarysearch.java
+++ searchbinary/binarysearch.java
@@ -2,8 +2,8 @@ package searchbinary;
 
 public class binarysearch {
      public static void main(String[] args) {
-        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
-        int target = 22;
+        int[] arr = {0,1,2,3,4,5,6,7,8,9};
+        int target = 0;
         int ans = binarySearch(arr, target);
         System.out.println(ans);
     }
@@ -12,7 +12,7 @@ public class binarysearch {
         int start=0;
         int end=arr.length-1;
        
-        while(start<end){
+        while(start<=end){
              int mid=start+(end-start)/2;
             
             if(arr[mid]<target){
