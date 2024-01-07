d72cf01b2f42b93c9d80c946bbf5af161446d07f
Vimalbharath <vimalbharath21@gmail.com>
2024-01-07 22:49:42 +0530

    confuse

--- searchbinary/jRotatedBinary.java
+++ searchbinary/jRotatedBinary.java
@@ -4,20 +4,19 @@ public class jRotatedBinary {
 
      public static void main(String[] args) {
         int[] arr = {3,4,5,6,7,8,9,0,1,2};
-        int target = 0;
-        int pivot = pivot(arr);
-        int ans=RBS(arr,target);
+        int target = 2;
+        int ans = RBS(arr,target);
+       
         System.out.println(ans);
     }
 
-    private static int RBS(int[] arr, int target) {
-        return 0;
-    }
+   
 
-    private static int pivot(int[] arr) {
+    private static int RBS(int[] arr,int target) {
         int start=0;
         int end=arr.length-1;
         int pivot=0;
+        int ans=-1;
         while (pivot==0){
             int mid=start+(end-start)/2;
             if(arr[mid]>arr[mid+1]){
@@ -35,9 +34,13 @@ public class jRotatedBinary {
                 start=mid+1;
             }
         }
+        pivot=pivot-1;
         
-
-        return pivot-1;
+        ans=binarySearch(arr, target, start, pivot);
+        if(ans==-1){
+            ans=binarySearch(arr, target, pivot+1,end);
+        }
+        return ans;
     }
 
     private static int binarySearch(int[] arr, int target,int start,int end) {
