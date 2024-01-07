0925e5f91ba5afc8dbdafb05a9a0cacbe2fb5970
Vimalbharath <vimalbharath21@gmail.com>
2024-01-07 20:51:40 +0530

    while == error

---
+++ searchbinary/hMountain.java
@@ -0,0 +1,25 @@
+package searchbinary;
+
+public class hMountain {
+
+     public static void main(String[] args) {
+        int[] arr = {0,1,2,3,4,5,6,7,8,9,8,7};
+        // int target = 0;
+        int ans = peakIndexInMountainArray(arr);
+        System.out.println(ans);
+    }
+     public static int peakIndexInMountainArray(int[] arr) {
+        int start=0;
+        int end=arr.length-1;
+        while(start==end){
+            int mid=start+(end-start)/2;
+            if(arr[mid+1]>arr[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
+        }
+        return start;
+    }
+}
