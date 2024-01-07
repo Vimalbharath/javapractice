d289927bb266b85a6da3252e5a8ff172a58d11d5
Vimalbharath <vimalbharath21@gmail.com>
2024-01-07 12:50:24 +0530

    out of bound in modulo

---
+++ searchbinary/eWrappedLeter.java
@@ -0,0 +1,19 @@
+package searchbinary;
+
+public class eWrappedLeter {
+     public char nextGreatestLetter(char[] letters, char target) {
+        int start=0;
+        int end=letters.length-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(letters[mid]<target){
+                start=mid+1;
+            }
+            if(letters[mid]>target){
+                end=mid-1;
+            }
+        }
+        return letters[start%(letters.length)-1];
+    }
+}
