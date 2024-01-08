b7d3c580c061110cae32a5b0029d0797a54e554a
Vimalbharath <vimalbharath21@gmail.com>
2024-01-08 07:59:18 +0530

    copy try success

--- searchbinary/jRotatedBinary.java
+++ searchbinary/jRotatedBinary.java
@@ -3,10 +3,10 @@ package searchbinary;
 public class jRotatedBinary {
 
      public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
-        int target = 1;
-        int ans = RBS(arr,target);
-       
+        int[] arr = {2,3,4,5,6,0,1};
+        int target = 3;
+        //int ans = RBS(arr,target);
+       int ans = findpivot(arr);
         System.out.println(ans);
     }
 
@@ -26,26 +26,24 @@ public class jRotatedBinary {
     private static int findpivot(int[] arr){
         int start=0;
         int end=arr.length-1;
-        int pivot=0;
         
-        while (pivot==0){
+        
+        while (start<=end){
             int mid=start+(end-start)/2;
-            if(arr[mid]>arr[mid+1]){
-                pivot=mid;
-                break;
+            if(arr[start]>arr[mid] &&arr[mid+1]<arr[mid]){
+                return mid;
             }
-             if(arr[mid]<arr[mid-1]){
-                pivot=mid;
-                break;
+             if(arr[start]>arr[mid]&&arr[mid-1]>arr[mid]){
+                return mid-1;
             }
-            if(arr[mid]<arr[start]){
+            if(arr[mid]<=arr[start]){
                 end=mid-1;
             }
-            if(arr[mid]>arr[start]){
+            else{
                 start=mid+1;
             }
         }
-        return pivot=pivot-1;
+        return -1;
     }
 
     private static int binarySearch(int[] arr, int target,int start,int end) {
