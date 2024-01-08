cbe5846eb6ada565ae0e070baa4a8bf1b85549dd
Vimalbharath <vimalbharath21@gmail.com>
2024-01-08 13:57:05 +0530

    row col matrix binary search

---
+++ searchbinary/kRowColMatrix.java
@@ -0,0 +1,20 @@
+package searchbinary;
+
+import java.util.Arrays;
+
+public class kRowColMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 38, 50}
+        };
+
+        System.out.println(Arrays.toString(search(arr, 49)));
+    }
+
+    private static int[] search(int[][] arr, int i) {
+        return null;
+    }
+}
