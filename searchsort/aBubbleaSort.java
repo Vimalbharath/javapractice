4451f9c0d2f391cc89ef82fb3618d94a4adce04c
Vimalbharath <vimalbharath21@gmail.com>
2024-01-09 13:32:55 +0530

    bubble sort easy with swapped

---
+++ searchsort/aBubbleaSort.java
@@ -0,0 +1,28 @@
+package searchsort;
+
+import java.util.Arrays;
+
+public class aBubbleaSort {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,0,1,-1,-2};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubble(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+        boolean swapped=false;    
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swapped=true;
+                }
+            }
+        if(!swapped){
+            break;
+        }    
+        }
+    }
+}
