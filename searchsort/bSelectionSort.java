ae9707657400ddb267239adbfcf9343213416b7c
Vimalbharath <vimalbharath21@gmail.com>
2024-01-09 14:08:21 +0530

    selection sort debug

---
+++ searchsort/bSelectionSort.java
@@ -0,0 +1,40 @@
+package searchsort;
+
+import java.util.Arrays;
+
+public class bSelectionSort {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,0,1,-1,-2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void selection(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            int start=0;
+            int last=arr.length-1-i;
+            int max=getMax(arr,start,last);
+            swap(arr,max,last);
+        }
+    }
+
+    private static void swap(int[] arr, int max, int last) {
+
+        int temp=arr[max];
+        arr[max]=arr[last];
+        arr[last]=temp;
+    }
+
+    private static int getMax(int[] arr, int start, int last) {
+        int max=arr[start];
+        for(int i=start;i<last;i++){
+            
+            if(arr[i]>=max){
+                max=arr[i];
+            }
+        }
+        return max;
+        
+
+    }
+}
