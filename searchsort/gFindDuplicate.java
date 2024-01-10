3b9d830c52d0d0713436a4c39707eaf54efa0305
Vimalbharath <vimalbharath21@gmail.com>
2024-01-10 19:32:15 +0530

    working for my case

--- searchsort/gFindDuplicate.java
+++ searchsort/gFindDuplicate.java
@@ -1,8 +1,10 @@
 package searchsort;
 
+//https://leetcode.com/problems/find-the-duplicate-number/
+
 public class gFindDuplicate {
     public static void main(String[] args) {
-        int[] nums={3,4,2,1,5};
+        int[] nums={3,4,1,1,5};
         miss(nums);
         //swap(nums,)
         System.out.println(miss(nums));
@@ -12,19 +14,17 @@ public class gFindDuplicate {
      int i=0;
        
         while(i<nums.length){
-        int correct=nums[i];
-        if(nums[i]<nums.length&&nums[i]!=nums[correct]){
+        int correct=nums[i]-1;
+        if(nums[i]!=nums[correct]){
             swap(nums,i,correct);
         }else{
+            return nums[i];
+        }
+       
             i++;
-        }
+    
        }
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=j){
-                return j;
-            }
-        }
-        return nums.length;
+        return -1;
     }
 
     private static void swap(int[] nums, int i,int j) {
