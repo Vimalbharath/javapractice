d9757c7954ef85ca6780a53a12e3563a29ce7edd
Vimalbharath <vimalbharath21@gmail.com>
2024-01-10 19:51:59 +0530

    format document

--- searchsort/gFindDuplicate.java
+++ searchsort/gFindDuplicate.java
@@ -4,36 +4,35 @@ package searchsort;
 
 public class gFindDuplicate {
     public static void main(String[] args) {
-        int[] nums={1,3,4,2,2};
+        int[] nums = { 1, 3, 4, 2, 2 };
         miss(nums);
-        //swap(nums,)
+        // swap(nums,)
         System.out.println(miss(nums));
     }
 
     private static int miss(int[] nums) {
-     int i=0;
-       
-        while(i<nums.length){
+        int i = 0;
 
-        if(nums[i]!=i+1){    
-        int correct=nums[i]-1;
-        if(nums[i]!=nums[correct]){
-            swap(nums,i,correct);
+        while (i < nums.length) {
+
+            if (nums[i] != i + 1) {
+                int correct = nums[i] - 1;
+                if (nums[i] != nums[correct]) {
+                    swap(nums, i, correct);
+                } else {
+                    return nums[i];
+                }
+            } else {
+                int a = i++;
+
+            }
         }
-        else{
-            return nums[i];
-        }}
-       else{
-           int a= i++;
-    
-       }
-    }
         return -1;
     }
 
-    private static void swap(int[] nums, int i,int j) {
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
