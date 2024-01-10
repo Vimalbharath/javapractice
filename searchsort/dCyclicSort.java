85c600d085a7031b777a954d2d054ebe964548e7
Vimalbharath <vimalbharath21@gmail.com>
2024-01-10 12:52:07 +0530

    debuged cyclic sort

--- searchsort/dCyclicSort.java
+++ searchsort/dCyclicSort.java
@@ -5,13 +5,25 @@ import java.util.Arrays;
 
 public class dCyclicSort {
      public static void main(String[] args) {
-        int[] arr={5,4,3,2,0,1,-1,-2};
+        int[] arr={5,4,3,2,1,};
         cycle(arr);
         //swap(arr,)
         System.out.println(Arrays.toString(arr));
     }
 
     private static void cycle(int[] arr) {
+        for(int i=0;i<arr.length-2;i++){
+            while (arr[i]!=i+1) {
+                swap(arr,i,arr[i]);
+            }
+
+        }
+    }
+
+    private static void swap(int[] arr, int i,int j) {
+        int temp=arr[i];
+        arr[i]=arr[j-1];
+        arr[j-1]=temp;
     }
 
 }
