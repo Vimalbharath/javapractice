458056b668a4bc07dfd6780612f92512c045c7cc
Vimalbharath <vimalbharath21@gmail.com>
2024-01-10 21:01:55 +0530

    duplicate and missing in array

--- searchsort/iSetMisMatch.java
+++ searchsort/iSetMisMatch.java
@@ -1,7 +1,40 @@
 package searchsort;
 //https://leetcode.com/problems/set-mismatch/
 
+import java.util.Arrays;
 
 public class iSetMisMatch {
     
+    
+    public static void main(String[] args) {
+        int[] nums={1,2,2,4};
+        miss(nums);
+        //swap(nums,)
+        System.out.println(Arrays.toString(miss(nums)));
+    }
+
+    private static int[] miss(int[] nums) {
+     int i=0;
+       
+        while(i<nums.length){
+        int correct=nums[i]-1;
+        if(nums[i]!=nums[correct]){
+            swap(nums,i,correct);
+        }else{
+            i++;
+        }
+       }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1){
+                return new int[] {j,j+1};
+            }
+        }
+        return new int[] {-1,-1};
+    }
+
+    private static void swap(int[] nums, int i,int j) {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
 }
