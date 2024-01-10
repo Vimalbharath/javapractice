6f5f86f36959029d23170181f86c08af436663f4
Vimalbharath <vimalbharath21@gmail.com>
2024-01-10 12:36:47 +0530

    swap with vale

---
+++ searchsort/dCyclicSort.java
@@ -0,0 +1,17 @@
+package searchsort;
+
+import java.util.Arrays;
+//if given in the question 1-N
+
+public class dCyclicSort {
+     public static void main(String[] args) {
+        int[] arr={5,4,3,2,0,1,-1,-2};
+        cycle(arr);
+        //swap(arr,)
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void cycle(int[] arr) {
+    }
+
+}
