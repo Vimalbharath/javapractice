6686fae80c6f349094057acedc7cae1d60fc8932
Vimalbharath <vimalbharath21@gmail.com>
2024-01-18 13:34:18 +0530

    binary search

---
+++ recursion/aintro/cBinSearch.java
@@ -0,0 +1,22 @@
+package recursion.aintro;
+
+public class cBinSearch {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,4,5,6,7,8,9};
+        int target = 0;
+        int ans = binarySearch(arr, target,0,9);
+        System.out.println(ans);
+    }
+
+    private static int binarySearch(int[] arr, int target,int start,int end) {
+        int mid=start+(end-start)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[mid]>target){
+            return binarySearch(arr, target, mid+1, end);
+        }
+        else
+        {return binarySearch(arr, target, start, mid-1);}
+    }
+}
