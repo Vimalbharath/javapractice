37ca950b373f192bcf9996a8b191df4d7675fa6d
Vimalbharath <vimalbharath21@gmail.com>
2024-01-18 13:23:12 +0530

    return n not 1

--- recursion/aintro/bfibbo.java
+++ recursion/aintro/bfibbo.java
@@ -2,13 +2,13 @@ package recursion.aintro;
 
 public class bfibbo {
     public static void main(String[] args) {
-       int num=fibbo(5);
+       int num=fibbo(4);
        System.out.println(num);
     }
 
     private static int fibbo(int n) {
         if(n<2){
-            return 1;
+            return n;
         }
         return fibbo(n-1)+fibbo(n-2);
     }
