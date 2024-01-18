e74df065b1b579ed4d697bd47ba0c305bde3e64e
Vimalbharath <vimalbharath21@gmail.com>
2024-01-18 13:40:20 +0530

    stop no condition

--- recursion/aintro/cBinSearch.java
+++ recursion/aintro/cBinSearch.java
@@ -4,11 +4,14 @@ public class cBinSearch {
     public static void main(String[] args) {
         int[] arr = {0,1,2,3,4,5,6,7,8,9};
         int target = 0;
-        int ans = binarySearch(arr, target,0,9);
+        int ans = binarySearch(arr, target,0,arr.length-1);
         System.out.println(ans);
     }
 
     private static int binarySearch(int[] arr, int target,int start,int end) {
+        if (start>end){
+            return -1;
+        }
         int mid=start+(end-start)/2;
         if(arr[mid]==target){
             return mid;
