b91880ca9c40f0d04b4c85039bbe013de0571375
Vimalbharath <vimalbharath21@gmail.com>
2024-01-18 13:14:56 +0530

    first try

--- recursion/aintro/bfibbo.java
+++ recursion/aintro/bfibbo.java
@@ -1,5 +1,16 @@
 package recursion.aintro;
 
 public class bfibbo {
+    public static void main(String[] args) {
+       int num=fibbo(5);
+       System.out.println(num);
+    }
+
+    private static int fibbo(int n) {
+        if(n<2){
+            return 1;
+        }
+        return fibbo(n-1)+fibbo(n-2);
+    }
     
 }
