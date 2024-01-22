80a9b5ba33c25a17be0c80844feaf381329f4e86
Vimalbharath <vimalbharath21@gmail.com>
2024-01-22 20:07:22 +0530

    whole number try

---
+++ maths/eBinarySQRT.java
@@ -0,0 +1,31 @@
+package maths;
+
+public class eBinarySQRT {
+    public static void main(String[] args) {
+        int n = 36;
+        int p = 3;
+
+        System.out.printf("%.3f", sqrt(n, p));
+    }
+
+    private static double sqrt(int n, int p) {
+        int s=0;
+        int e=n;
+        double root=0.0;
+
+        while(s<=e){
+            int m=s+(e-s)/2;
+
+            if(m*m==e){
+                return m;
+            }
+            else if(m*m>e){
+                e=m-1;
+            }else{
+                s=m+1;
+                root=m;
+            }
+        }
+      return root;  
+    }
+}
