d212e19d017e6d45ed6e34e09518a251bc1293a3
Vimalbharath <vimalbharath21@gmail.com>
2024-01-22 22:46:30 +0530

    recursion calling itself

---
+++ recursion/bsimple/bproduct2.java
@@ -0,0 +1,15 @@
+package recursion.bsimple;
+
+public class bproduct2 {
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+
+    private static int fact(int n) {
+        if(n==1){
+            return 1;
+        }
+        return n*fact(n-1);
+
+    }
+}
