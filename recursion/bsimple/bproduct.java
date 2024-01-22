da14315fb4dcf2bc8c6cf4da3cf1dec0d2a2471a
Vimalbharath <vimalbharath21@gmail.com>
2024-01-22 22:28:36 +0530

    while using void we get correct ans

--- recursion/bsimple/bproduct.java
+++ recursion/bsimple/bproduct.java
@@ -2,17 +2,19 @@ package recursion.bsimple;
 
 public class bproduct {
      public static void main(String[] args) {
-        System.out.println(product(5,1));
+      product(5,1);
     }
 
-    private static int product(int n,int result) {
-        int ans=result;
+    private static void product(int n,int result) {
+      //  int ans=result;
         if(n==0){
-           System.out.println(ans);
-            return ans;
+            // here printing the correct ans while return it print original
+         System.out.println(result);
+         return;
+
         }
-        result=ans*n;
+        result=result*n;
         product(n-1,result);
-        return result;
+        
     }
 }
