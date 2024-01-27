b24963ca0941ce270d61102ffee4c8039e064bb2
Vimalbharath <vimalbharath21@gmail.com>
2024-01-27 09:24:52 +0530

    not a recursion

--- recursion/bsimple/ereverse.java
+++ recursion/bsimple/ereverse.java
@@ -2,13 +2,15 @@ package recursion.bsimple;
 
 public class ereverse {
      public static void main(String[] args) {
-        System.out.println(revdigits(12345));
+        System.out.println(revdigits1(12345));
     }
+    static int sum=0;
 
-    private static int revdigits(int n) {
+    private static int revdigits1(int n) {
         if(n==0){
-            return 0;
+            return sum;
         }
-        return revdigits(n/10) +10 *  n%10;
+        sum=sum*10+n%10;
+        return revdigits1(n/10) ;
     }
 }
