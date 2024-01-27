5fd8bb14a3aeb7e1813c77f3021b13052dae92f8
Vimalbharath <vimalbharath21@gmail.com>
2024-01-27 14:32:46 +0530

    without argument new arraylist

---
+++ recursion/carrays/linearsearch4.java
@@ -0,0 +1,29 @@
+package recursion.carrays;
+
+import java.util.ArrayList;
+
+public class linearsearch4 {
+     public static void main(String[] args) {
+        int[] arr={0,1,2,3,3,3,3,3,6,7,8};
+        int target=3;
+        System.out.println(linear4(arr,0,target));
+    }
+    
+    
+
+    private static ArrayList<Integer> linear4(int[] arr,int index,int target) {
+        ArrayList<Integer> list=new ArrayList<>();
+        if(arr.length==index){
+            return list;
+        }
+
+        if ( arr[index]==target){
+            list.add(index);
+        }
+        ArrayList<Integer> ans=linear4(arr,index+1,target);
+        list.addAll(ans);
+        return list;
+
+
+    }
+}
