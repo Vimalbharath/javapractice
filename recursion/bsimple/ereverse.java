b9f02cf1b61742c530bd059cd5fda6604e8021b0
Vimalbharath <vimalbharath21@gmail.com>
2024-01-27 09:44:16 +0530

    new reverse using recursion helper

--- recursion/bsimple/ereverse.java
+++ recursion/bsimple/ereverse.java
@@ -2,7 +2,18 @@ package recursion.bsimple;
 
 public class ereverse {
      public static void main(String[] args) {
-        System.out.println(revdigits1(12345));
+        System.out.println(revdigits2(12345));
+    }
+    private static int revdigits2(int n) {
+        int noofdigits=(int) Math.log10(n)+1;
+        return helper(n,noofdigits);
+    }
+    private static int helper(int n, int noofdigits) {
+        if(n%10==n){
+            return n;
+        }
+        int rem=n%10;
+        return rem*(int)Math.pow(10, noofdigits-1)+helper(n/10,noofdigits-1);
     }
     static int sum=0;
 
