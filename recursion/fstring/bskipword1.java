b0863741a5630ea207da40c1162a29cb54b4ce16
Vimalbharath <vimalbharath21@gmail.com>
2024-01-29 16:31:28 +0530

    need to edit

---
+++ recursion/fstring/bskipword1.java
@@ -0,0 +1,22 @@
+package recursion.fstring;
+
+public class bskipword1 {
+    public static void main(String[] args) {
+        System.out.println(skipapple("abappplecdbbcbg"));
+
+    }
+
+    private static String skipapple(String up) {
+       if(up.isEmpty()){
+       
+        return"";
+       }
+       char ch=up.charAt(0);
+       if(up.startsWith("apple")){
+        return skipapple(up.substring(5));
+       }
+       else{
+        return ch+skipapple(up.substring(1));
+       }
+    }
+}
