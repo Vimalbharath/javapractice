d0e07fae4f15f4324c3d4ce25f462d2f53329ca7
Vimalbharath <vimalbharath21@gmail.com>
2024-01-29 15:49:11 +0530

    try error

---
+++ recursion/fstring/askipletter1.java
@@ -0,0 +1,22 @@
+package recursion.fstring;
+
+public class askipletter1 {
+    public static void main(String[] args) {
+        skip("","abcdbbcbg");
+
+    }
+
+    private static void skip(String p,String up) {
+       if(up.isEmpty()){
+        System.out.println(p);
+        return;
+       }
+       char ch=up.charAt(0);
+       if(ch=='b'){
+        skip(p,up.substring(1));
+       }
+       else{
+        skip(p,ch+up.substring(1));
+       }
+    }
+}
