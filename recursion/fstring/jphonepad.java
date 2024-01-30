72564818da6e8a41725b837297f96e5dc0216cb7
Vimalbharath <vimalbharath21@gmail.com>
2024-01-30 13:01:33 +0530

    question mark

---
+++ recursion/fstring/jphonepad.java
@@ -0,0 +1,21 @@
+package recursion.fstring;
+
+public class jphonepad {
+    public static void main(String[] args) {
+        permutation("","12");
+    }
+
+    private static void permutation(String p, String up) {
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        int digit=up.charAt(0)+'0';
+
+        
+        for(int i=(digit-1)*3;i<digit*3;i++){
+            char ch=(char)('a'+i);
+            permutation(p+ch,up.substring(1));
+        }
+    }
+}
