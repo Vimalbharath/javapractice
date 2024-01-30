c4e1b370bd78357a0f65ccc3aa265bc8d7667dde
Vimalbharath <vimalbharath21@gmail.com>
2024-01-30 13:39:51 +0530

    empty list fail

--- recursion/fstring/lphonepadlc.java
+++ recursion/fstring/lphonepadlc.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class lphonepadlc {
     public static void main(String[] args) {
-        System.out.println(permutation("","12"));
+        System.out.println(permutation("","23"));
     }
 
     private static ArrayList<String> permutation(String p, String up) {
@@ -15,9 +15,19 @@ public class lphonepadlc {
         }
         ArrayList<String> ans = new ArrayList<>();
         int digit=up.charAt(0)-'0';
+        int j=(digit-2)*3;
+        int len=j+3;
+        if(digit>7){
+            j=j+1;
+        }
+        if(digit==7 ||digit==9 ){
+            len=len+1;
+        }
 
         
-        for(int i=(digit-1)*3;i<digit*3;i++){
+
+        
+        for(int i=j;i<len;i++){
             char ch=(char)('a'+i);
             ans.addAll(permutation(p+ch,up.substring(1)));
         }
