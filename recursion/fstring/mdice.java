2088930d8862ab3c4a15d27264c7909518575c7f
Vimalbharath <vimalbharath21@gmail.com>
2024-01-30 14:47:54 +0530

    stackover flow

---
+++ recursion/fstring/mdice.java
@@ -0,0 +1,17 @@
+package recursion.fstring;
+
+public class mdice {
+    public static void main(String[] args) {
+        dice("",4);
+    }
+
+    private static void dice(String p, int target) {
+       if(target==0){
+        System.out.println(p);
+        return;
+       }
+       for(int i=1;i<=6&&i<=4;i++){
+            dice(p+i,target);
+       }
+    }
+}
