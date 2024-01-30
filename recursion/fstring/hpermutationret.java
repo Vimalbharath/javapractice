663ca230e3b68e6184c311ff763dadb45c6888b1
Vimalbharath <vimalbharath21@gmail.com>
2024-01-30 11:47:05 +0530

    copy paste good

--- recursion/fstring/hpermutationret.java
+++ recursion/fstring/hpermutationret.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class hpermutationret {
     public static void main(String[] args) {
-        permutationret("","abc");
+        System.out.println(permutationret("","abc"));
     }
 
     private static ArrayList<String> permutationret(String p, String up) {
@@ -13,11 +13,13 @@ public class hpermutationret {
             list.add(p);
             return list;
         }
+        ArrayList<String> ans = new ArrayList<>();
         char ch=up.charAt(0);
         for(int i=0;i<=p.length();i++){
             String f = p.substring(0, i);
             String s = p.substring(i, p.length());
-            return (permutationret(f+ch+s,up.substring(1)));
+            ans.addAll (permutationret(f+ch+s,up.substring(1)));
         }
+        return ans;
     }
 }
