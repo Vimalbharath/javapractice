3108a924e825069b1942fc406f63fe3df1fadf85
Vimalbharath <vimalbharath21@gmail.com>
2024-02-06 19:44:30 +0530

    upto display correct

--- linkedlist/bmain.java
+++ linkedlist/bmain.java
@@ -3,7 +3,12 @@ package linkedlist;
 public class bmain {
     public static void main(String[] args) {
         aLL linked=new aLL();
-        linked.addFirst(4;)
+        linked.addFirst(4);
+        linked.addFirst(5);
+        linked.addFirst(6);
+        linked.addFirst(7);
+        linked.addFirst(8);
+        linked.display();
     }
    
 }
