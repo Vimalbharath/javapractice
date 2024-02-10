e0c39528a019eb0ea64d0bf2ba28442bb85afe84
Vimalbharath <vimalbharath21@gmail.com>
2024-02-10 20:22:53 +0530

    can't

--- linkedlist/erecinsert.java
+++ linkedlist/erecinsert.java
@@ -6,6 +6,7 @@ public class erecinsert extends aLL {
         Node node=head;
         if(index==0){
             Node temp=new Node(val);
+            temp.next=node.next;
             node.next=temp;
             return temp;
         }
