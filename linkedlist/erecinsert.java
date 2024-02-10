c56bfb23989a60f065201d40d1c870f150cb56e2
Vimalbharath <vimalbharath21@gmail.com>
2024-02-10 19:33:04 +0530

    pass ret blank

---
+++ linkedlist/erecinsert.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+public class erecinsert extends aLL {
+
+    public void insertrec(int val,int index){
+        Node node=getNode(index);
+        if(index==0){
+            Node temp=new Node(val);
+            node.=node.next;
+            return;
+        }
+
+    }
+}
