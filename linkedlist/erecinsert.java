6037bea66dd383a3e0cb22f764605f13e76779a7
Vimalbharath <vimalbharath21@gmail.com>
2024-02-10 20:12:05 +0530

    infinite

--- linkedlist/erecinsert.java
+++ linkedlist/erecinsert.java
@@ -14,4 +14,26 @@ public class erecinsert extends aLL {
 
 
     }
+     public static void main(String[] args) {
+        erecinsert linked=new erecinsert();
+        linked.addFirst(4);
+        linked.addFirst(5);
+        linked.addFirst(6);
+        linked.addFirst(7);
+        linked.addFirst(8);
+        linked.addLast(3);
+        //System.out.println(linked.getSize());
+        linked.add(9,2);
+        //System.out.println(linked.getSize());
+        linked.display();
+        //System.out.println(linked.getSize());
+        System.out.println(linked.deletefirst());
+        //System.out.println(linked.getSize());
+        System.out.println(linked.deleteLast());
+        //System.out.println(linked.getSize());
+        linked.display();
+        System.out.println(linked.delete(2));
+        linked.insertrec(600, 2);
+        linked.display();
+    }
 }
