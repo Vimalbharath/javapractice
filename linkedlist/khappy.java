e0f079223ca4e11541103496e633971dca935815
Vimalbharath <vimalbharath21@gmail.com>
2024-02-11 22:26:45 +0530

    Time limit exceed

---
+++ linkedlist/khappy.java
@@ -0,0 +1,29 @@
+package linkedlist;
+
+public class khappy {
+    public static boolean isHappy(int n) {
+        int f=0;
+        int s=0;
+
+        do{
+        f=sum(n);
+        s=sum(sum(n));
+        if(f==1){
+            return true;
+        }
+        }while(f!=s);
+    return false;
+    }
+    public static int sum(int n){
+        int ans=0;
+        while(n>0){
+            int a=n%10;
+            ans=ans+(a*a);
+            n=n/10;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+    }
+}
