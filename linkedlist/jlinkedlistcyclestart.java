440de5a8f38056be4d308ff6daaa43bc514fbb5c
Vimalbharath <vimalbharath21@gmail.com>
2024-02-11 14:25:48 +0530

    success leetcode on single try

---
+++ linkedlist/jlinkedlistcyclestart.java
@@ -0,0 +1,47 @@
+package linkedlist;
+//https://leetcode.com/problems/linked-list-cycle/
+
+public class jlinkedlistcyclestart {
+
+     public ListNode detectCycle(ListNode head) {
+        ListNode f = head;
+        ListNode s = head;
+        int length=lengthCycle(head);
+        if(length==0){
+            return null;
+        }
+        while(length!=0){
+            f=f.next;
+            length--;
+        }
+        while(f!=s){
+            f = f.next;
+            s = s.next;
+        }
+        return s;
+        
+    }
+
+    public int lengthCycle(ListNode head) {
+        ListNode f = head;
+        ListNode s = head;
+        
+        while (f != null && f.next != null) {
+
+            f = f.next.next;
+            s = s.next;
+            if (f == s) {
+                //f = head;
+                int count = 0;
+                ListNode temp=s;
+                do {
+                    //f = f.next;
+                    temp = temp.next;
+                    count = count + 1;
+                } while (temp != s);
+                return count;
+            }
+        }
+        return 0;
+    }
+}
