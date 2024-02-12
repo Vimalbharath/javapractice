5f9c4c51e101772d660ce087660182ed30e38355
Vimalbharath <vimalbharath21@gmail.com>
2024-02-12 15:42:45 +0530

    push is good

--- stacksqueue/acustomstack.java
+++ stacksqueue/acustomstack.java
@@ -3,16 +3,38 @@ package stacksqueue;
 public class acustomstack {
     private int [] data;
     private static final int DEFAULT_SIZE=10;
-    private int size;
 
+    int ptr=-1;
+    //private int size;
+
+    //  public  acustomstack(){
+    //     this.DEFAULT_SIZE;
+    // }
+
+    // public acustomstack(int size) {
+    //     this.size = size;
+    // }
      public  acustomstack(){
-        this.DEFAULT_SIZE;
+        this(DEFAULT_SIZE);
     }
 
     public acustomstack(int size) {
-        this.size = size;
+        this.data = new int[size];
     }
 
+    public boolean push(int val){
+        if(isfull()){
+            System.out.println("Stack is full");
+            return true;
+        }
+        ptr++;
+        data[ptr]=val;
+        return false;
+    }
+
+    public boolean isfull(){
+        return ptr==data.length-1;
+    }
    
 
 }
