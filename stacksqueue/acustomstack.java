05637dda1947fde517dae0dc3dbf114595d4a7e2
Vimalbharath <vimalbharath21@gmail.com>
2024-02-12 15:32:05 +0530

    default error

---
+++ stacksqueue/acustomstack.java
@@ -0,0 +1,18 @@
+package stacksqueue;
+
+public class acustomstack {
+    private int [] data;
+    private static final int DEFAULT_SIZE=10;
+    private int size;
+
+     public  acustomstack(){
+        this.DEFAULT_SIZE;
+    }
+
+    public acustomstack(int size) {
+        this.size = size;
+    }
+
+   
+
+}
