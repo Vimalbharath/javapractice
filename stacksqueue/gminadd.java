dc7703653c755f6384f05bb2606264ca3282b292
Vimalbharath <vimalbharath21@gmail.com>
2024-02-14 13:53:20 +0530

    video copy paste

---
+++ stacksqueue/gminadd.java
@@ -0,0 +1,26 @@
+package stacksqueue;
+
+//https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+
+import java.util.Stack;
+
+public class gminadd {
+     public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.size();
+
+    }
+
+}
