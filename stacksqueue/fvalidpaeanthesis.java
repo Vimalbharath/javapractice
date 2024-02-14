5778d10cf8437fec25c938769ff2da754dcaccbf
Vimalbharath <vimalbharath21@gmail.com>
2024-02-14 13:22:27 +0530

    return mistake

---
+++ stacksqueue/fvalidpaeanthesis.java
@@ -0,0 +1,34 @@
+package stacksqueue;
+
+import java.util.Stack;
+
+public class fvalidpaeanthesis {
+     public boolean isValid(String s) {
+        Stack <Character> stack= new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(ch=='{'|| ch=='['||ch=='('){
+                stack.push(ch);
+            }else{
+                if(ch=='}'){
+                    if(stack.isEmpty()||stack.pop()!='}'){
+                        return false;
+                    }
+                }
+                if(ch==']'){
+                    if(stack.isEmpty()||stack.pop()!=']'){
+                        return false;
+                    }
+                }
+                if(ch==')'){
+                    if(stack.isEmpty()||stack.pop()!=')'){
+                        return false;
+                    }
+                }
+
+            }
+
+            
+        }
+         return stack.isEmpty();
+    }
+}
